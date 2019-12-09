135476a8c123460a0c24b625b5c527f2de2a87ac
Ouarkoub Camelea <ouarkoubc@b11p4.fil.univ-lille1.fr>
2019-12-09 16:48:39 +0100

    detail fait mais visu à revoir

--- projetAndroidCamelea/app/src/main/java/com/example/projetandroidcamelea/PokemonDetail.java
+++ projetAndroidCamelea/app/src/main/java/com/example/projetandroidcamelea/PokemonDetail.java
@@ -5,10 +5,18 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.projetandroidcamelea.Common.Common;
+import com.example.projetandroidcamelea.Model.Pokemon;
 
 
 /**
@@ -17,6 +25,9 @@ import android.view.ViewGroup;
 public class PokemonDetail extends Fragment {
 
     static PokemonDetail instance;
+    ImageView pokemon_image;
+    TextView pokemon_name,pokemon_height,pokemon_weight;
+    RecyclerView  recycler_type,recycler_weakness;
 
     public static PokemonDetail getInstance() {
         if (instance == null){
@@ -36,6 +47,41 @@ public class PokemonDetail extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokemon_detail, container, false);
+        View itemView = inflater.inflate(R.layout.fragment_pokemon_detail, container, false);
+
+        Pokemon pokemon;
+        //Get position
+        if(getArguments().get("number") == null){
+            pokemon = Common.commonPokemonList.get(getArguments().getInt("position"));
+
+        }
+        else
+            pokemon =null;
+        pokemon_image = (ImageView)itemView.findViewById(R.id.pokemon_image);
+        pokemon_name = (TextView) itemView.findViewById(R.id.pokemon_name);
+        pokemon_height = (TextView) itemView.findViewById(R.id.height);
+        pokemon_weight = (TextView) itemView.findViewById(R.id.weight);
+        recycler_type =(RecyclerView) itemView.findViewById(R.id.recycler_type);
+        recycler_type.setHasFixedSize(true);
+        recycler_type.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
+        recycler_weakness =(RecyclerView) itemView.findViewById(R.id.recycler_weakness);
+        recycler_weakness.setHasFixedSize(true);
+        recycler_weakness.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
+
+        setDetailPokemon(pokemon);
+
+        return itemView;
+
+    }
+
+    //Putting the data of pokemon onto the detail fragment
+    private void setDetailPokemon(Pokemon pokemon) {
+        //Load image
+        Glide.with(getActivity()).load(pokemon.getImg()).into(pokemon_image);
+
+        //Loading text data
+        pokemon_name.setText((pokemon.getName()));
+        pokemon_height.setText("Height : " +pokemon.getHeight());
+        pokemon_weight.setText("Weight :"+ pokemon.getWeight());
     }
 }
